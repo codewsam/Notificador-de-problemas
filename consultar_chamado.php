280512a50d4838a6f5ddcf9a51a2c684707bb0a5
codewsam <araujosamuel097@gmail.com>
2024-09-03 19:11:24 -0300

    to emocionado,que trabalho deu, mas os chamados tao aparecendo na tela que devem agr

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -3,6 +3,21 @@
 require_once "validar_acesso.php";
 ?>
 
+<?php
+
+//chamados
+$chamados = array();
+
+//abrir arquivo .hd
+$arquivo = fopen('arquivo.hd', 'r');
+
+while(!feof($arquivo)){
+  $registro = fgets($arquivo);
+  $chamados[] = $registro;
+}
+fclose($arquivo);
+
+?>
 
 
 
@@ -47,23 +62,28 @@ require_once "validar_acesso.php";
             
             <div class="card-body">
               
+              <?php foreach($chamados as $chamado) { ?>
+
+                <?php
+                $chamado_dados = explode ('#', $chamado);
+                if(Count($chamado_dados)< 3){
+                  continue;
+                }
+                
+
+                ?>
+
+
               <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+                  <h5 class="card-title"> <?= $chamado_dados[0]?> </h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1]?></h6>
+                  <p class="card-text"><?= $chamado_dados[2]?></p>
 
                 </div>
               </div>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <?php } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
